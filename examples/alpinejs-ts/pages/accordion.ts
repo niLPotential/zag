1f9751ede0d01df0abfd7ccaafafab01132c2bad
niLPotential <niLPotential@proton.me>
2025-07-15 04:56:56 +0000

    feat(alpinejs): add client script

--- examples/alpinejs-ts/pages/accordion.ts
+++ examples/alpinejs-ts/pages/accordion.ts
@@ -1,6 +1,23 @@
 import Alpine from "alpinejs"
+import * as accordion from "@zag-js/accordion"
+import { useMachine, normalizeProps } from "@zag-js/alpinejs"
 
 // @ts-ignore
 window.Alpine = Alpine
+
+Alpine.data("accordion", () => {
+  const service = useMachine(accordion.machine, {
+    id: "1",
+    dir: "ltr",
+  })
+  return {
+    get api() {
+      return accordion.connect(service, normalizeProps)
+    },
+    init() {
+      service.init()
+    },
+  }
+})
 
 Alpine.start()
