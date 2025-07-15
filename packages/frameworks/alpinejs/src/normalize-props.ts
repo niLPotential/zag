1037db5ca1680fe1c98de72a9849c4efd8482d13
niLPotential <niLPotential@proton.me>
2025-07-15 04:42:54 +0000

    feat(alpinejs): add normalize props

--- packages/frameworks/alpinejs/src/normalize-props.ts
+++ packages/frameworks/alpinejs/src/normalize-props.ts
@@ -1,3 +1,14 @@
 import { createNormalizer } from "@zag-js/types"
 
-export const normalizeProps = createNormalizer((props) => props)
+export const normalizeProps = createNormalizer((props) => {
+  const normalized: Record<string, any> = {}
+  for (const key in props) {
+    if (!props[key]) continue
+    if (key.startsWith("on")) {
+      normalized[`@${key.substring(2).toLowerCase()}`] = props[key]
+    } else {
+      normalized[":" + key] = () => props[key]
+    }
+  }
+  return normalized
+})
