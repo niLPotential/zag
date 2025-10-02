f492882e8fc1df9b5de8d40bfb0cdc627328f395
Lee Chang Min <niLPotential@proton.me>
2025-10-02 05:36:57 +0000

    feat(alpine): add checkbox magic

--- examples/alpine-ts/src/checkbox.ts
+++ examples/alpine-ts/src/checkbox.ts
@@ -30,5 +30,46 @@ export default function (Alpine: Alpine) {
     } else if (value === "hidden-input") {
       Alpine.bind(el, (Alpine.$data(el).api as checkbox.Api).getHiddenInputProps)
     }
+  }).before("bind")
+
+  Alpine.magic("checkbox", (el) => {
+    const { api } = Alpine.$data(el) as { api: checkbox.Api }
+
+    return {
+      get checked() {
+        return api.checked
+      },
+      get disabled() {
+        return api.disabled
+      },
+      get indeterminate() {
+        return api.indeterminate
+      },
+      get focused() {
+        return api.focused
+      },
+      get checkedState() {
+        return api.checked
+      },
+
+      setChecked: api.setChecked,
+      toggleChecked: api.toggleChecked,
+
+      get rootProps() {
+        return api.getRootProps()
+      },
+      get labelProps() {
+        return api.getLabelProps()
+      },
+      get controlProps() {
+        return api.getControlProps()
+      },
+      get indicatorProps() {
+        return api.getIndicatorProps()
+      },
+      get hiddenInputProps() {
+        return api.getHiddenInputProps()
+      },
+    }
   })
 }
