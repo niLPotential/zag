4aa80f4c42934acbff87d8a988cf2d03d8d533bc
Lee Chang Min <niLPotential@proton.me>
2025-10-08 14:53:57 +0900

    feat(alpine): add avatar example

--- examples/alpine-ts/src/server/+app.tsx
+++ examples/alpine-ts/src/server/+app.tsx
@@ -8,6 +8,9 @@ import { src as accordionSrc } from "client:script/accordion"
 import { AngleSlider } from "../pages/angle-slider"
 import { src as angleSliderSrc } from "client:script/angle-slider"
 
+import { Avatar } from "../pages/avatar"
+import { src as avatarSrc } from "client:script/avatar"
+
 const app = new App()
 
 // app.use((c, next) => {
@@ -22,6 +25,10 @@ app.get("/accordion", (c) => {
 app.get("/angle-slider", (c) => {
   c.head(<script type="module" src={angleSliderSrc.module.at(0)}></script>)
   c.page(<AngleSlider />)
+})
+app.get("/avatar", (c) => {
+  c.head(<script type="module" src={avatarSrc.module.at(0)}></script>)
+  c.page(<Avatar />)
 })
 
 export default { fetch: app.fetch }
