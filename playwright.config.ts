d0f05880e822f191dcaa8b4955ed362b7d69a3a1
nilpotential <nilpotential@proton.me>
2025-10-28 16:37:30 +0900

    chore: add color-picker example

--- playwright.config.ts
+++ playwright.config.ts
@@ -59,6 +59,7 @@ export default defineConfig({
     "checkbox.e2e.ts",
     "clipboard.e2e.ts",
     "collapsible.e2e.ts",
+    "color-picker.e2e.ts",
     "date-picker.e2e.ts",
   ],
   fullyParallel: !CI,
