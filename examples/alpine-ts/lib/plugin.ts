1240d10453b5d9ff056f76e1f49932414bf85a3c
Chang Min Lee <nilpotential@proton.me>
2026-03-21 22:17:35 +0900

    refactor: expose api only as magic

--- examples/alpine-ts/lib/plugin.ts
+++ examples/alpine-ts/lib/plugin.ts
@@ -22,12 +22,9 @@ export function usePlugin<T extends MachineSchema>(
     gridCollection?: (options: any) => any
   },
 ) {
-  const _x_snake_case = "_x_" + name.replaceAll("-", "_")
-
   return function (Alpine: Alpine) {
     Alpine.directive(name, (el, { expression, value, modifiers }, { effect, evaluateLater }) => {
-      const modifier = modifiers.at(0)
-      const _modifier = modifier ? "_" + modifier : ""
+      const apiName = joinCamelCase([...name.split("-"), modifiers.at(0) ?? ""])
       if (!value) {
         const evaluateProps = evaluateLater<Partial<T["props"]> | (() => Partial<T["props"]>)>(expression)
         const userPropsRef = Alpine.reactive({ value: {} as Partial<T["props"]> | (() => Partial<T["props"]>) })
@@ -35,11 +32,11 @@ export function usePlugin<T extends MachineSchema>(
           evaluateProps((props) => (userPropsRef.value = props))
         })
         const machine = new AlpineMachine(component.machine, userPropsRef)
-        Alpine.magic(joinCamelCase([...name.split("-"), modifier ?? "", "service"]), () => machine.service)
+        Alpine.magic(apiName + "Service", () => machine.service)
+        Alpine.magic(apiName, () => component.connect(machine.service, normalizeProps))
         Alpine.bind(el, {
           "x-data"() {
             return {
-              [_x_snake_case + _modifier]: component.connect(machine.service, normalizeProps),
               init() {
                 machine.init()
               },
@@ -47,9 +44,6 @@ export function usePlugin<T extends MachineSchema>(
                 machine.destroy()
               },
             }
-          },
-          "x-effect"() {
-            ;(this as any)[_x_snake_case + _modifier] = component.connect(machine.service, normalizeProps)
           },
         })
       } else if (value === "collection") {
@@ -88,7 +82,7 @@ export function usePlugin<T extends MachineSchema>(
 
         Alpine.bind(el, () => {
           const propsRef = Alpine.reactive(
-            usePartProps((props) => (Alpine.$data(el) as any)[_x_snake_case + _modifier][getPartProps](props)),
+            usePartProps((props) => (Alpine.$data(el) as any)["$" + apiName][getPartProps](props)),
           ) as Record<string, any>
 
           return {
@@ -101,18 +95,12 @@ export function usePlugin<T extends MachineSchema>(
             }, {}),
             "x-effect"() {
               evaluateProps((props) => {
-                Object.assign(propsRef, (this as any)[_x_snake_case + _modifier][getPartProps](props))
+                Object.assign(propsRef, (this as any)["$" + apiName][getPartProps](props))
               })
             },
           }
         })
       }
     }).before("bind")
-    Alpine.magic(joinCamelCase(name.split("-")), () => {
-      return function (modifier?: string) {
-        // @ts-ignore
-        return (this as any)[_x_snake_case + (modifier ? "_" + modifier : "")]
-      }
-    })
   }
 }
