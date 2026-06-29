c8fe1576dadb4f76a388883e4183a77503307309
Chang Min LEE <nilpotential@proton.me>
2026-06-29 15:35:03 +0900

    fix(e2e-vue): tooltip/basic

--- examples/nuxt-ts/app/pages/tooltip/basic.vue
+++ examples/nuxt-ts/app/pages/tooltip/basic.vue
@@ -1,23 +1,40 @@
 <script setup>
 import * as tooltip from "@zag-js/tooltip"
 import { normalizeProps, useMachine } from "@zag-js/vue"
-import { computed } from "vue"
 
-const service = useMachine(tooltip.machine, {
-  id: useId(),
-  positioning: { sameWidth: true },
-})
+const id = "tip-1"
+const id2 = "tip-2"
+const service = useMachine(tooltip.machine, { id })
+const service2 = useMachine(tooltip.machine, { id: id2 })
 
 const api = computed(() => tooltip.connect(service, normalizeProps))
+const api2 = computed(() => tooltip.connect(service2, normalizeProps))
 </script>
 
 <template>
-  <div style="padding: 40px">
-    <button v-bind="api.getTriggerProps()">Hover me</button>
-    <Teleport to="#teleports">
-      <div v-if="api.open" v-bind="api.getPositionerProps()">
-        <div v-bind="api.getContentProps()">Tooltip</div>
-      </div>
-    </Teleport>
-  </div>
+  <main class="tooltip">
+    <div class="root">
+      <button :data-testid="`${id}-trigger`" v-bind="api.getTriggerProps()">Hover me</button>
+      <Teleport to="#teleports">
+        <div v-bind="api.getPositionerProps()">
+          <Presence class="tooltip-content" :data-testid="`${id}-tooltip`" v-bind="api.getContentProps()">
+            Tooltip
+          </Presence>
+        </div>
+      </Teleport>
+
+      <button :data-testid="`${id2}-trigger`" v-bind="api2.getTriggerProps()">Over me</button>
+      <Teleport to="#teleports">
+        <div v-bind="api2.getPositionerProps()">
+          <Presence class="tooltip-content" :data-testid="`${id2}-tooltip`" v-bind="api2.getContentProps()">
+            Tooltip 2
+          </Presence>
+        </div>
+      </Teleport>
+    </div>
+  </main>
+  <Toolbar>
+    <StateVisualizer :state="service" label="Tooltip 1" />
+    <StateVisualizer :state="service2" label="Tooltip 2" />
+  </Toolbar>
 </template>
